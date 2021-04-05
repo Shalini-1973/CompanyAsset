7572d9f0a2b74fd968a6874fcabee1b32c179468
Shalini-1973 <75874335+Shalini-1973@users.noreply.github.com>
2021-04-05 14:01:01 +0530

    Update EmployeeServiceImpl.java
    comment added

--- CompanyAsset/src/main/java/com/rmgx/assignment/service/EmployeeServiceImpl.java
+++ CompanyAsset/src/main/java/com/rmgx/assignment/service/EmployeeServiceImpl.java
@@ -18,7 +18,7 @@ import com.rmgx.assignment.repo.EmployeeRepo;
 public class EmployeeServiceImpl implements EmployeeService {
 
 	private static final org.slf4j.Logger log = 
-		    org.slf4j.LoggerFactory.getLogger(EmployeeServiceImpl.class);
+		    org.slf4j.LoggerFactory.getLogger(EmployeeServiceImpl.class);	// declared an instance of Slf4j logger to create logs
 	
 	@Autowired
 	private EmployeeRepo emplRepo;
@@ -28,7 +28,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	
 	@Override
 	public Employee addEmployee(EmployeeDto employeeDto) {
-		Employee employee = new Employee();
+		Employee employee = new Employee();		// created an object of Employee using default constructor
 		employee.setName(employeeDto.getName());
 		employee.setDesignation(employeeDto.getDesignation());
 		return emplRepo.save(employee);
@@ -36,7 +36,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	@Override
 	public Employee searchEmployeeById(long id) {
-		Employee employee = emplRepo.findById(id).get();
+		Employee employee = emplRepo.findById(id).get();   // used Jpa findById method for finding an employee
 		return employee;
 	}
 
@@ -52,14 +52,14 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	@Override
 	public Employee assignAsset(AssignDto assignDto) {
-		Employee employee = emplRepo.findById(assignDto.getEmployeeId()).get();
-		Asset asset = assetRepo.findById(assignDto.getAssetId()).get();
+		Employee employee = emplRepo.findById(assignDto.getEmployeeId()).get();	// get the employee of the id  coming in assignDto
+		Asset asset = assetRepo.findById(assignDto.getAssetId()).get();		// get the asset of the id coming in assetDto
 		Set<Asset> assets = employee.getAssets();
 		if(asset!=null && employee!=null && (asset.getAssignment_status().equals("Available")||asset.getAssignment_status().equals("Recovered"))) {
-		asset.setEmployee(employee);
+		asset.setEmployee(employee);			// validate the asset and employee and status of the asset
 		asset.setConditionnotes(assignDto.getCondtionNote());
-		asset.setPurchasedate(ZonedDateTime.now().toInstant().toEpochMilli());
-		asset.setAssignment_status("Assigned");
+		asset.setPurchasedate(ZonedDateTime.now().toInstant().toEpochMilli());	// update purchased date to current date
+		asset.setAssignment_status("Assigned");					// update the asset status to assigned
 		assets.add(asset);
 		}
 		employee.setAssets(assets);
@@ -70,18 +70,18 @@ public class EmployeeServiceImpl implements EmployeeService {
 	
 	@Override
 	public Employee recoverAsset(RecoverDto recoverDto) {
-		Employee employee = emplRepo.findById(recoverDto.getEmployeeId()).get();
-		Asset asset = assetRepo.findById(recoverDto.getAssetId()).get();
-		Set<Asset> assets = employee.getAssets();
-		if(asset!=null && employee!=null) {
+		Employee employee = emplRepo.findById(recoverDto.getEmployeeId()).get();	// get the employee by id coming in recoverDto
+		Asset asset = assetRepo.findById(recoverDto.getAssetId()).get();		// get the asset by it coming in recoverDto
+		Set<Asset> assets = employee.getAssets();					// get all the asset assigned to the employee
+		if(asset!=null && employee!=null) {						// validated the employee and asset
 			
 			asset.setEmployee(null);
 			asset.setConditionnotes(null);
 			asset.setPurchasedate(null);
 			asset.setAssignment_status("Recovered");
-			for(Asset myasset:assets) {
+			for(Asset myasset:assets) {						// searching for asset in list of assigned asset
 				if(myasset.getId()==asset.getId()) {
-					boolean remove = assets.remove(myasset);
+					boolean remove = assets.remove(myasset);		// after searching removed it from the list
 					if(remove) {
 						log.info("asset recovered successfully");
 					}else {
