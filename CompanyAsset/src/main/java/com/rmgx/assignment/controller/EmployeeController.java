6d2c49d0817beaad94057fbcbb68f65bb7d3873f
Shalini-1973 <75874335+Shalini-1973@users.noreply.github.com>
2021-04-05 14:09:27 +0530

    Update EmployeeController.java
    comment added

--- CompanyAsset/src/main/java/com/rmgx/assignment/controller/EmployeeController.java
+++ CompanyAsset/src/main/java/com/rmgx/assignment/controller/EmployeeController.java
@@ -19,36 +19,36 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService service;
 	
-	@PostMapping("/addEmployee")
+	@PostMapping("/addEmployee")				//handle a request for adding an employee
 	public ResponseEntity<Employee> addEmployee(@RequestBody EmployeeDto employeeDto){
-		Employee addedEmployee = service.addEmployee(employeeDto);
+		Employee addedEmployee = service.addEmployee(employeeDto); // a service method called for adding an employee to the database
 		return ResponseEntity.ok(addedEmployee);
 	}
 	
-	@PostMapping("/getEmployeeById")
+	@PostMapping("/getEmployeeById")				// handle the request for getting an employee details
 	public ResponseEntity<Employee> getEmployeeById(@RequestParam(value="EmployeeId") long id){
 		
-		Employee searchedEmployee = service.searchEmployeeById(id);
+		Employee searchedEmployee = service.searchEmployeeById(id); // a service method called for fetching the employee data from database
 		
 		return ResponseEntity.ok(searchedEmployee);
 	}
 	
-	@PostMapping("/deleteEmployeeById")
+	@PostMapping("/deleteEmployeeById")				// handles the request for deleting the employee
 	public ResponseEntity<String> deleteEmployeeById(@RequestParam(value="EmployeeId") long id){
-		String deleteEmployee = service.deleteEmployeeId(id);;
+		String deleteEmployee = service.deleteEmployeeId(id);;	 // method called to validate and delete an employee from the database
 		return ResponseEntity.ok(deleteEmployee);
 	}
 	
-	@PostMapping("/assignAsset")
+	@PostMapping("/assignAsset")		//handle request to assign an asset to an employee
 	public ResponseEntity<Employee> assignAsset(@RequestBody AssignDto assignDto){
-		Employee assignedAsset = service.assignAsset(assignDto);
+		Employee assignedAsset = service.assignAsset(assignDto);	// a service method call for assigning an asset to the employee and store it to database.
 		return ResponseEntity.ok(assignedAsset);
 	}
 	
-	@PostMapping("/recoverAsset")
+	@PostMapping("/recoverAsset")			//handle a request to recover a assigned asset to an employee
 	public ResponseEntity<Employee> recoverAsset(@RequestBody RecoverDto recoverDto){
 		
-		Employee recoveredAsset = service.recoverAsset(recoverDto);
+		Employee recoveredAsset = service.recoverAsset(recoverDto); // a service method call for recovering an asset to the employee and store it to database.
 		return ResponseEntity.ok(recoveredAsset);
 	}
 }
