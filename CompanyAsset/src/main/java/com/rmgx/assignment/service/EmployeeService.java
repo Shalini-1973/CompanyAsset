5c18c521be9e98f2d0ae1886e62be056d445455c
Shalini-1973 <75874335+Shalini-1973@users.noreply.github.com>
2021-04-05 13:53:30 +0530

    Update EmployeeService.java
    comment added

--- CompanyAsset/src/main/java/com/rmgx/assignment/service/EmployeeService.java
+++ CompanyAsset/src/main/java/com/rmgx/assignment/service/EmployeeService.java
@@ -7,14 +7,14 @@ import com.rmgx.assignment.entity.Employee;
 
 public interface EmployeeService {
 	
-	public Employee addEmployee(EmployeeDto employeeDto);
+	public Employee addEmployee(EmployeeDto employeeDto);	// method for adding an employee
 	
-	public Employee searchEmployeeById(long id);
+	public Employee searchEmployeeById(long id);		// method for searching an employee bye name
 	
-	public String deleteEmployeeId(long id);
+	public String deleteEmployeeId(long id);		// method for deleting an employee
 	
-	public Employee assignAsset(AssignDto assignDto);
+	public Employee assignAsset(AssignDto assignDto);	// method for assigning an asset to the employee
 	
-	public Employee recoverAsset(RecoverDto recoverDto);
+	public Employee recoverAsset(RecoverDto recoverDto);	// method to recover asset assigned to an employee
 	
 }
